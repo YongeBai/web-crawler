49ef349159a95e2f1e4cd0294998214db5316551
YongeBai <yongebai@gmail.com>
2024-05-21 19:49:35 -0400

    getUrlsFromHtml()

--- crawl.js
+++ crawl.js
@@ -1,11 +1,30 @@
+import { JSDOM } from 'jsdom';
 
 function normalizeURL(url) {
-    let urlObj  = new(url)
-    let domainAndPath = urlObj.hostname + urlObj.pathname
+    let urlObj  = new URL(url);
+    let domainAndPath = urlObj.hostname + urlObj.pathname;
     if (domainAndPath.endsWith('/')) {
-        domainAndPath = domainAndPath.slice(0, -1)
+        domainAndPath = domainAndPath.slice(0, -1);
     }
-    return domainAndPath
+    return domainAndPath;
 }
 
-export { normalizeURL };
+function getUrlsFromHtml(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody, {
+        url: baseURL
+    })
+    let urls = [];
+    for (let a of dom.window.document.querySelectorAll('a')) {
+        let href = a.getAttribute('href');
+        if (href) {
+            if (href.includes('://')) {
+                urls.push(href);
+            } else {
+                urls.push(baseURL + href);
+            }
+        }
+    }
+    return urls;
+}
+
+export { normalizeURL, getUrlsFromHtml };
--- crawl.test.js
+++ crawl.test.js
@@ -1,14 +1,37 @@
 import { test, expect} from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getUrlsFromHtml } from "./crawl.js";
 
-test('test http', () => {
-    expect(normalizeURL('http://www.example.com')).toBe('www.example.com');    
-})
-
-test('test https', () => {
+// normalizeURL tests
+test('test removing protocol', () => {
+    expect(normalizeURL('http://www.example.com')).toBe('www.example.com');        
     expect(normalizeURL('https://www.example.com')).toBe('www.example.com');
 })
 
-test('test path has no trailing slash', () => {
+test('test removing trailing slash', () => {
+    expect(normalizeURL('http://www.example.com/')).toBe('www.example.com');
     expect(normalizeURL('http://www.example.com/path/')).toBe('www.example.com/path');
 })
+
+// test getUrlsFromHtml()
+test('get one absolute url', () => {
+    expect(getUrlsFromHtml('<a href="http://www.google.com">link</a>', 'http://www.example.com')).toEqual(['http://www.google.com']);
+})
+test('get relative url', () => {
+    expect(getUrlsFromHtml(`
+    <html>
+    <body>
+        <a href='/path'>Go to Boot.dev</a>
+    </body>
+</html>`, 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev/path']);
+})
+test('get relative and absolute urls', () => {
+    expect(getUrlsFromHtml(`
+    <html>
+    <body>
+        <a href='/path'>Go to Boot.dev</a>
+        <a href='http://www.google.com'>Google</a>
+    </body>`, 'http://www.example.com'
+    )).toEqual(['http://www.example.com/path', 'http://www.google.com']);
+})
+
+
